f0ea420ca43b906b59d9a231dd34b3483f311d86
waimea-eharrisbrocklesby <eharrisbrocklesby@waimea.school.nz>
2026-03-19 12:24:08 +1300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+/**
+ * =====================================================================
+ * Programming Project for NCEA Level 2, Standard 91896
+ * ---------------------------------------------------------------------
+ * Project Name:   PROJECT NAME HERE
+ * Project Author: PROJECT AUTHOR HERE
+ * GitHub Repo:    https://github.com/waimea-eharrisbrocklesby/kotlin-l2-game
+ * ---------------------------------------------------------------------
+ * Notes:
+ * PROJECT NOTES HERE
+ * =====================================================================
+ */
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+
+
+fun main() {
+    println("Hello World!")
 }
+
+
