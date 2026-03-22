78465fd61b2f724d48bd9cc066747a8282500841
waimea-eharrisbrocklesby <eharrisbrocklesby@waimea.school.nz>
2026-03-23 10:59:01 +1300

    play spaces
    I created the play spaces as their own function that i call to later. I also added a few other small things like the game title. and a verification asking if you wish to play

--- src/Main.kt
+++ src/Main.kt
@@ -11,10 +11,53 @@
  * =====================================================================
  */
 
+fun createSpace(space: MutableList<String>) {
+    for (i in 1..12) {
+        space.add(" ")
+    }
+}
 
+fun printspace(space: List<String>) {
+    println("   1     2     3     4     5     6     7     8     9     10    11    12".blue())
+    println("╔═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╗".green())
+    print("║ ".green())
+    for (space in space) {
+        print(space.padEnd(3))
+        print(" ║ ".green())
+    }
+    println()
+    println("╚═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╝".green())
+}
 
 fun main() {
-    println("Hello World!")
+    val space = mutableListOf<String>()//play space difine thingy
+    createSpace(space)
+
+    println("╔══════════════════╗")
+    println("║  Chain reaction! ║ ")
+    println("╚══════════════════╝")
+    println( )
+    println( )
+
+    println("Do You whish to play? [Y] [N]")
+    print("Choice: ")
+    val choice = readln().first().uppercaseChar()
+    when (choice) {
+        'Y' -> {
+            println()
+            println()
+            printspace(space)
+
+            println()
+            println("Player (X) Start: ")
+            print("where would you like to play: ")
+            // rest of game
+        }
+        'N' -> {
+            println("That's Okay, Come back another time!")
+        }
+    }
+
 }
 
 
